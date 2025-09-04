1350e2ec6ea3f5d4c8f47886afcb6453ffd07b30
Abdul Arfan <abdul.arfan@gmail.com>
2025-09-04 21:14:54 +0700

    first class

---
+++ Helloapplication/app/src/main/java/id/co/kmbmicro/helloapplication/SampleAcitivy.java
@@ -0,0 +1,34 @@
+package id.co.kmbmicro.helloapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SampleAcitivy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sample_acitivy);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            // Tambahkan kode untuk menangani klik tombol di sini
+            // send intent to change activity to MainActivity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
